53f147bdc1118c2a69790c9f340f3745753cee1a
GianniPio <giannipiopellegrino00@gmail.com>
2021-12-13 11:30:29 +0100

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,23 @@
 <template>
-  <div id="app">
-    <HelloWorld/>
-  </div>
+  <SeoHeader/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SeoHeader from './components/SeoHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SeoHeader
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/SeoHeader.vue
@@ -0,0 +1,33 @@
+<template>
+    <!-- hedaer -->
+    <header>
+
+        <!-- logo -->
+        <div>
+            <!-- <img src="../../src/assets/images/logo_seo_w_2x.png" alt="Logo della pagina"> -->
+        </div>
+
+        <!-- Menu -->
+        <div>
+
+        </div>
+
+    </header>
+
+    
+</template>
+
+<script>
+export default {
+  name: 'SeoHeader',
+}
+</script>
+
+<style scoped lang="scss">
+
+header {
+    background-color: purple;
+    height: 600px;
+}
+
+</style>
