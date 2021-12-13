4017dda308f32f12555aa1f4a8a13662028d4584
GianniPio <giannipiopellegrino00@gmail.com>
2021-12-13 11:39:15 +0100

    add footer

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div>
     <SeoHeader/>
     <SeoMain/>
+    <SeoFooter/>
   </div>
 </template>
 
 <script>
 import SeoHeader from './components/SeoHeader.vue'
 import SeoMain from './components/SeoMain.vue'
+import SeoFooter from './components/SeoFooter.vue'
 
 export default {
   name: 'App',
   components: {
     SeoHeader,
-    SeoMain
+    SeoMain,
+    SeoFooter,
   }
 }
 </script>
---
+++ src/components/SeoFooter.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer>
+
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'SeoFooter',
+}
+</script>
+
+<style scoped lang="scss">
+footer {
+    height: 300px;
+    background-color: green;
+}
+</style>
